3e6cba76b17554fe25fabf8bafed44728606fb2f
Andrew Wheeler <awheeler294@gmail.com>
2016-04-11 08:19:50 -0600

    initial shop API framework

---
+++ classes/ShopAPI.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Andrew
+ * Date: 4/9/2016
+ * Time: 10:32 AM
+ */
+class ShopAPI extends API
+{
+    protected $User;
+
+    public function __construct($request, $origin)
+    {
+        parent::__construct($request);
+
+//        // Abstracted out for example
+//        $APIKey = new APIKey();
+//        $User = new User();
+//
+//        if (!array_key_exists('apiKey', $this->request)) {
+//
+//            throw new Exception('No API Key provided');
+//
+//        } else if (!$APIKey->verifyKey($this->request['apiKey'], $origin)) {
+//
+//            throw new Exception('Invalid API Key');
+//
+//        } else if (array_key_exists('token', $this->request) && !$User->get('token', $this->request['token']))
+//        {
+//
+//            throw new Exception('Invalid User Token');
+//        }
+//
+//        $this->User = $User;
+    }
+
+
+    protected function registerESL()
+    {
+        if ($this->method == 'POST') {
+            return "POST " . print_r($this->request, true);
+        } else if ($this->method == 'GET') {
+            return "GET " . print_r($this->request, true);
+        }
+    }
+
+    protected function createDeliveryRequest()
+    {
+        if ($this->method == 'POST') {
+            return "POST " . print_r($this->request, true);
+        } else if ($this->method == 'GET') {
+            return "GET " . print_r($this->request, true);
+        }
+    }
+}
---
+++ shop/api/api.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andrew
+ * Date: 4/9/2016
+ * Time: 10:29 AM
+ */
+// Requests from the same server don't have a HTTP_ORIGIN header
+if (!array_key_exists('HTTP_ORIGIN', $_SERVER)) {
+
+    $_SERVER['HTTP_ORIGIN'] = $_SERVER['SERVER_NAME'];
+
+}
+
+try {
+
+    $API = new ShopAPI($_REQUEST['request'], $_SERVER['HTTP_ORIGIN']);
+    echo $API->processAPI();
+
+} catch (Exception $e) {
+
+    echo json_encode(Array('error' => $e->getMessage()));
+
+}
